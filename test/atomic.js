4c80610986b46d5320c7ab81069818376c14baee
John Otander <johnotander@gmail.com>
2017-11-09 15:03:39 -0700

    Finish basic atomic integration

--- test/atomic.js
+++ test/atomic.js
@@ -6,50 +6,18 @@ const config = [
     name: 'Box',
     type: 'div',
     props: {
-      bg: 'tomato'
+      bg: 'tomato',
+      ml: 2
     },
     style: {
-      'text-transform': 'uppercase'
+      'text-transform': 'uppercase',
+      'text-align': 'left'
     }
-  },
-  {
-    name: 'Text',
-    type: 'p',
-    props: {
-      py: 5
-    },
-    style: {
-      'text-align': 'right',
-      'line-height': '3',
-      'font-size': '16px'
-    }
-  },
-  {
-    name: 'Hello',
-    imports: ['Box', 'Text'],
-    jsx: '<Box><Text /></Box>'
-  },
-  {
-    name: 'Beep',
-    type: 'Box',
-    props: {},
-    style: {}
-  },
-  {
-    name: 'Flex',
-    type: 'Box',
-    props: {
-      m: 2
-    },
-    style: {
-      display: 'flex',
-    },
-    system: ['alignItems', 'justifyContent', 'flexDirection', 'flexWrap']
   }
 ]
 
 const theme = {
-  space: [4, 8, 16, 32, 64, 128, 256]
+  space: [2, 4, 8, 16, 32, 64]
 }
 
 test('adds expected classes to elements', t => {
